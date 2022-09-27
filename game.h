6f5a69dc6ae00d75b6548a98b41968ce3a0a8b3a
東 愁汰 <fko2247067@stu.o-hara.ac.jp>
2022-09-27 12:26:19 +0900

    テキストの追加

--- game.h
+++ game.h
@@ -5,7 +5,7 @@ namespace Game
 	// ウィンドウモード設定
 	constexpr bool kWindowMode = true;
 	// ウィンドウ名
-	const char* const kTitleText = "テンプレの米満";
+	const char* const kTitleText = "僕に気づいた君は賢いね";
 	// ウィンドウサイズ
 	constexpr int kScreenWidth = 1280;
 	constexpr int kScreenHeight = 720;
